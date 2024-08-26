cfefe3a5e9fb049ad9f9ddbbb2a0738a0e7b7a65
Álvaro José Cofré Pérez <59652090+alvarocofre@users.noreply.github.com>
2024-08-26 00:22:52 -0400

    Create ex3.3.c

---
+++ 06_Punteros/ex3.3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void swap(int *a, int *b){
+	int *temp;
+	temp = a; 
+	a = b; 
+	b = temp;
+}
+
+int main(){
+	int x=2, y=3;
+	swap(&x,&y);
+	printf("x: %d, y: %d\n", x, y); //¿Qué imprime?
+	return 0;
+}
